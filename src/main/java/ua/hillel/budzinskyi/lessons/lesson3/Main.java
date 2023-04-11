cafcc104c926c9cf3c2090bdc6acd6b54996d250
Budzinskyi Artem <artem.farobin@gmail.com>
2023-04-11 19:59:31 +0300

    test push

--- src/main/java/ua/hillel/budzinskyi/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/budzinskyi/lessons/lesson3/Main.java
@@ -3,5 +3,9 @@ package ua.hillel.budzinskyi.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!");
+        System.out.println("TEST");
+        System.out.println("Homework");
+        System.out.println("Hello world!!!!");
+        System.out.println("Hello world!!!!");
     }
 }
