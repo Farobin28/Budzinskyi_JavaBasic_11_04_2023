3c71c0d9f8a4cfedb845200b92d5da1364502571
Budzinskyi Artem <artem.farobin@gmail.com>
2023-04-17 16:13:20 +0300

    4 lesson homework

---
+++ src/main/java/ua/hillel/budzinskyi/lessons/lesson4/Main.java
@@ -0,0 +1,15 @@
+package ua.hillel.budzinskyi.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 1;
+        int num3 = 4;
+        int volume;
+        volume = num1*num2*num3;
+        System.out.println("Об'єм піраміди: " + volume);
+        int length;
+        length = num1+num2+num3;
+        System.out.println("Сумарна кількість: " + length);
+    }
+}
